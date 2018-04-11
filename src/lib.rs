14d9bc9e7756a45d1e77ad9779640342361fddd9
Sam Rijs <srijs@airpost.net>
2018-04-11 18:33:23 +0200

    Bump version to v0.1.17 (#321)

--- src/lib.rs
+++ src/lib.rs
@@ -89,7 +89,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/tokio-core/0.1.16")]
+#![doc(html_root_url = "https://docs.rs/tokio-core/0.1.17")]
 #![deny(missing_docs)]
 #![deny(warnings)]
 #![cfg_attr(test, allow(deprecated))]
